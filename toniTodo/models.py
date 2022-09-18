08eb8b301dd0566bc695e0e37be4c8b9067db79c
Toni Tunes <tonitunes963@gmail.com>
2022-09-18 09:59:09 +0100

    new functionalities added

--- toniTodo/models.py
+++ toniTodo/models.py
@@ -11,3 +11,31 @@ class ToDo(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+    
+    
+#     class Profile(models.Model):
+#     first_name = models.CharField(max_length=500)
+#     last_name = models.CharField(max_length=500)
+#     email = models.EmailField(max_length=300, unique=True)
+#     password = models.CharField(max_length=500)
+#     profile_pic = models.ImageField(upload_to="static/images/profile_picture/", blank=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+# class Category(models.Model):
+#     pass
+
+# class PostModel(models.Model):
+#     title = models.CharField( max_length=5000)
+#     slug = models.SlugField()
+#     description = models.TextField()
+#     image = models.ImageField( upload_to="static/images/blog_post_image/", blank=False)
+#     status_type = (
+#         ('Pub', 'Published'),
+#         ('UnPub', 'Unpublished'),
+#     )
+#     status = models.CharField(choices=status_type, max_length= 12)
+    
+#     def __str__(self):
+#         return self.title
